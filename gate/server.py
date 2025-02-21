27e2fc8a8329badaaaa2a549a325eb6e74a5ef9a
Mykyta <75797773+Hampandoo@users.noreply.github.com>
2025-02-21 21:34:31 +0200

    Update server.py

--- gate/server.py
+++ gate/server.py
@@ -6,23 +6,19 @@ from app.config import settings
 
 app = FastAPI(title="Gateway")
 
-# Налаштування CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Дозволяє всі джерела (налаштуй для безпеки)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Підключення роутів
 app.include_router(router)
 
-# Глобальна обробка помилок
 # @app.exception_handler(Exception)
 # async def exception_handler(request, exc):
 #     return await handle_error(exc)
 
-# Запуск сервера
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=settings.GATEWAY_PORT)
